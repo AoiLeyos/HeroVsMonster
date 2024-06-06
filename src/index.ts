ef2ba6bd6fce3477c2101fd001a3fad965bb9b94
romain <romdz01@gmail.com>
2024-06-06 14:08:23 +0200

    feat(index.ts)create class personnages with constructor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
 class personnages {
+    name: string;
+    health: number;
+    healthMax: number;
 
+    constructor(name: string, health: number) {
+        this.name = name;
+        this.health = health;
+        this.healthMax = health;
+    }
+
+    beAlive(): boolean {
+        return this.health > 0;
+    }
+
+    gethealth(): number {
+        return this.health;
+    }
+
+    getname(): string {
+        return this.name;
+    }
 }
 
 class Hero {
