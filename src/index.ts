f6ee9a8a11362368a6c0a7e5ef878ad87f6a45a9
romain <romdz01@gmail.com>
2024-06-07 17:06:50 +0200

    fix(index.ts)import default for classMonster & classHero

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
+import Hero from "./class/classHero";
+import Monster from "./class/classMonster";
+
 const attackBtn = document.getElementById('attack-btn');
 const healBtn = document.getElementById('heal-btn');
 const resetBtn = document.getElementById('reset-btn');
 const hero = new Hero("Jacques le Paladin", 100);
 const monster = new Monster("Gollum", 100)
 
-import Hero from "./class/classHero";
 
-import Monster from "./class/classMonster";
 
 function displayHealhStatus() {
-    console.log(`${hero.getHealth()}`)
+    console.log(hero.getHealth())
+    console.log(monster.getHealth())
 }
 
 attackBtn?.addEventListener('click', () => {
     hero.Hit(monster);
-    monster.Hit(hero);
     monster.getHealth();
-    hero.getHealth();
+    setTimeout(() => {
+        monster.Hit(hero);
+        hero.getHealth();
+    }, 1500)
+
     displayHealhStatus();
 })
 
