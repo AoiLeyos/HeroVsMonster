4a57a5353888a8c105da78dbd88f4a54522b2a68
romain <romdz01@gmail.com>
2024-06-07 14:33:14 +0200

    feat(classCharacter)change name & isolated in another file for lisibility

---
+++ src/class/classCharacter.ts
@@ -0,0 +1,28 @@
+class Character {
+    protected damage: number;
+    protected name: string;
+    protected health: number;
+
+    constructor(name: string, health: number) {
+        this.name = name;
+        this.health = health;
+        this.damage = Math.floor(Math.random() * 21);
+    }
+
+    public Hit(target: Personnage) {
+        target.health -= this.damage;
+    }
+
+
+    isAlive(): boolean {
+        return this.health > 0
+    }
+
+    getHealth(): number {
+        return this.health;
+    }
+
+    getName(): string {
+        return this.name;
+    }
+}
