23cf9ce27e922eb2dc9561e8493dd9ae9156e296
romain <romdz01@gmail.com>
2024-06-07 14:34:00 +0200

    feat(classHero.ts)isolated in another file for lisibility

---
+++ src/class/classHero.ts
@@ -0,0 +1,24 @@
+class Hero extends Character {
+    private heal: number;
+
+    constructor(name: string, health: number) {
+        super(name, health);
+        this.heal = Math.floor(Math.random() * 21)
+    }
+
+    getHealth(): number {
+        return this.health;
+    }
+
+    setHealth(value: number) {
+        this.health = value;
+    }
+
+
+    Heal() {
+
+        if (this.health > 100) {
+            return 100
+        }
+    }
+}
